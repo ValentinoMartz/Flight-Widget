413d06d90002a7bfe8898e728c2c705651045fdd
ValentinoMartz <valentinomartinez55@gmail.com>
2022-10-13 17:01:48 -0300

    done

--- client/src/components/TableCell.jsx
+++ client/src/components/TableCell.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import TableLetter from "./TableLetter";
 
-const TableCell = () => {
-  return <div>TableCell</div>;
+const TableCell = ({ word }) => {
+  return (
+    <td>
+      {Array.from(word).map((letter, index) => (
+        <TableLetter key={index} letter={letter} index={index} />
+      ))}
+    </td>
+  );
 };
 
 export default TableCell;
--- client/src/components/TableRow.jsx
+++ client/src/components/TableRow.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import TableCell from "./TableCell";
 
-const TableRow = () => {
-  return <div>TableRow</div>;
+const TableRow = ({ flight }) => {
+  const words = Object.values(flight);
+
+  console.log(words);
+  return (
+    <tr>
+      <td>✈</td>
+      {words?.map((word, _index) => (
+        <TableCell key={_index} word={word} />
+      ))}
+    </tr>
+  );
 };
 
 export default TableRow;
